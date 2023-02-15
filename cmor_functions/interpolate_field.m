a3c1fab3931c5f309d1cfbdf2ccc0e26f2af1e6c
nadavis-ncar <63112962+nadavis-ncar@users.noreply.github.com>
2023-02-15 15:30:07 -0700

    Update interpolate_field.m
    Interp can now handle singular dimension (single level) interpolation.

--- cmor_functions/interpolate_field.m
+++ cmor_functions/interpolate_field.m
@@ -13,15 +13,10 @@ if nargin>4
    do_native=1;
 end
 
-
-
 %Set up output field
 field_size=size(field);
 field_size(interp_dim)=length(dim.interp);
 field_out=zeros(field_size);
-if do_native==1
-   field_out_local=zeros(size(field_out,3),size(field_out,4));
-end
 
 if interp_dim==2
    loop_num=3;
@@ -35,22 +30,17 @@ for i=1:size(field,1)
    disp([sprintf('%0.2f',100*i/size(field,1)),'% complete'])
    for j=1:size(field,loop_num)
       if do_native==1
-         size(field_out)
-size(field)
-size(field_out_local)
          if length(size(field_out))==3
             parfor k=1:size(field,3)
-               field_out_local(k)=interp1(squeeze(ps(i,j,k))*b+a,field(i,j,k,:),...
+               field_out(i,j,k)=interp1(squeeze(ps(i,j,k))*b+a,squeeze(field(i,j,k,:)),...
                                   dim.interp,'linear',missing_value);
             end
-            field_out(i,j,:)=field_out_local;
          else
             parfor k=1:size(field,3)
-               field_out_local(k,:)=interp1(squeeze(ps(i,j,k))*b+a,field(i,j,k,:),...
+               field_out(i,j,k,:)=interp1(squeeze(ps(i,j,k))*b+a,field(i,j,k,:),...
                                   dim.interp,'linear',missing_value);
             end
          end
-         field_out(i,j,:,:)=field_out_local;
      else
          fieldsize=length(size(field_out));
          switch fieldsize
