d3a983a3c3ec542e67d6c7d292182c2b8c22c89f
nadavis-ncar <63112962+nadavis-ncar@users.noreply.github.com>
2023-02-25 09:16:54 -0700

    Update omega_to_w.m
    Need to parallelize

--- cmor_functions/omega_to_w.m
+++ cmor_functions/omega_to_w.m
@@ -3,5 +3,18 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function data=omega_to_w(data,ps,a,b)
 
-pres=calculate_pressure(ps,a,b);
-data.native.value=9.81*(pres./(287*data.temp)).*data.native.value;
+inner_loop_1=size(data,2);
+inner_loop_2=size(data,3);
+inner_loop_3=size(data,4);
+
+for i=1:size(data,1)
+   data_local=squeeze(data(i,:,:,:));
+   ps_local=squeeze(ps(i,:,:));
+   data_out_local=zeros(size(data_local));
+   for j=1:inner_loop_1
+      for t=1:inner_loop_3
+         data_out_local(j,:,t)=(9.81*(a(:)'+b(:)'*squeeze(ps_local(j,t)))/287).*squeeze(data_local(j,:,t));
+      end
+   end
+   data(i,:,:,:)=data_out_local;
+end
