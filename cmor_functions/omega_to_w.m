e64f42783ed361a07b14819453fca7c88c18bc84
nadavis-ncar <63112962+nadavis-ncar@users.noreply.github.com>
2023-03-03 09:05:19 -0700

    Update omega_to_w.m

--- cmor_functions/omega_to_w.m
+++ cmor_functions/omega_to_w.m
@@ -3,16 +3,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function data=omega_to_w(data,ps,a,b)
 
-inner_loop_1=size(data,2);
-inner_loop_2=size(data,3);
-inner_loop_3=size(data,4);
-
-for i=1:size(data,1)
+parfor i=1:size(data,1)
    data_local=squeeze(data(i,:,:,:));
    ps_local=squeeze(ps(i,:,:));
    data_out_local=zeros(size(data_local));
-   for j=1:inner_loop_1
-      for t=1:inner_loop_3
+   for j=1:size(data_local,1)
+      for t=1:size(data_local,3)
          data_out_local(j,:,t)=(9.81*(a(:)'+b(:)'*squeeze(ps_local(j,t)))/287).*squeeze(data_local(j,:,t));
       end
    end
