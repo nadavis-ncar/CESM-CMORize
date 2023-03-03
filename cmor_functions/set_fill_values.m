fc7308c20470e5171a63e91b899f07defe1d68d7
nadavis-ncar <63112962+nadavis-ncar@users.noreply.github.com>
2023-03-03 09:06:15 -0700

    Create set_fill_values.m

---
+++ cmor_functions/set_fill_values.m
@@ -0,0 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Sets all NaN, Inf, and erroneously large values to the fill value
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function var_out=set_fill_values(var_out,missing_value)
+
+var_out.native.value(isinf(var_out.native.value))=missing_value;
+var_out.native.value(isnan(var_out.native.value))=missing_value;
+var_out.native.value(abs(var_out.native.value)>missing_value)=...
+               sign(var_out.native.value(abs(var_out.native.value)>missing_value))*missing_value;
