9265d094611833c9afdb6e0c1d973fdfb0554637
nadavis-ncar <63112962+nadavis-ncar@users.noreply.github.com>
2023-03-03 09:02:37 -0700

    Update convert_units.m

--- cmor_functions/convert_units.m
+++ cmor_functions/convert_units.m
@@ -4,14 +4,26 @@
 function data=convert_units(data,ps,a,b,conversion)
 
 if strcmp(conversion,'number_density_to_molar_mixing_ratio')
-   pres=calculate_pressure(ps,a,b);
-
-   rho=pres./(287*data.native_secondary.value);
-   n_air=(6.02e23/0.028)*rho;
-
-   data.native.value=(100^3)*data.native.value./n_air;
+   parfor i=1:size(data,1)
+      data_local=squeeze(data(i,:,:,:));
+      ps_local=squeeze(ps(i,:,:));
+      data_local_out=zeros(size(data_local));
+      for j=1:size(data_local,1)
+         for k=1:size(data_local,2)
+            for t=1:size(data_local,3)
+               data_local_out(j,k,t)=(100^3)*squeeze(data_local(j,k,t))/((6.02e23/0.028)*...
+                                        (a(k)+b(k)*squeeze(ps_local(j,t)))/287);
+            end
+         end
+      end
+      data(i,:,:,:)=data_local_out;
+   end
 elseif strcmp(conversion,'molar_mixing_ratio_to_DU')
-   data.native.value=data.native.value*(287*273.15/1e5);
+   data=data*(287*273.15/1e5);
 else
    error('unknown command passed to convert_units')
 end
+
+%Future work: use json with value/key pairs to determine conversions for a broader array of options; 
+%have call option for no supplied conversion (just check). We can plan ahead 
+%and ensure that var_dictionary loads temp when there is a chemical integral.
