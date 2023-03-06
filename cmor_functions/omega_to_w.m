fdec8baf6a962f9211346fab25018004ebbce451
nadavis-ncar <63112962+nadavis-ncar@users.noreply.github.com>
2023-03-06 11:18:48 -0700

    Update omega_to_w.m
    Need to parallelize (some sticky issues, maybe related to nested loops).

--- cmor_functions/omega_to_w.m
+++ cmor_functions/omega_to_w.m
@@ -3,14 +3,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function data=omega_to_w(data,ps,a,b)
 
-parfor i=1:size(data,1)
-   data_local=squeeze(data(i,:,:,:));
-   ps_local=squeeze(ps(i,:,:));
-   data_out_local=zeros(size(data_local));
-   for j=1:size(data_local,1)
-      for t=1:size(data_local,3)
-         data_out_local(j,:,t)=(9.81*(a(:)'+b(:)'*squeeze(ps_local(j,t)))/287).*squeeze(data_local(j,:,t));
+for i=1:size(data,1)
+   for j=1:size(data,2)
+      for t=1:size(data,4)
+         data(i,j,:,t)=(9.81*(a(:)+b(:)*squeeze(ps(i,j,t)))/287).*squeeze(data(i,j,:,t));
       end
    end
-   data(i,:,:,:)=data_out_local;
 end
